db2bc5f9cbc9e1a79f6830993f9689e024bd771e
Peter <pedamain@gmx.at>
2020-11-13 20:01:54 +0100

    restoring

--- studyapp-frontend/src/ContentArea.js
+++ studyapp-frontend/src/ContentArea.js
@@ -2,28 +2,17 @@ import React, { PureComponent } from 'react'
 import Recipe from './Recipe'
 import Recipiecard from './Recipecard'
 import Styles from './style.css'
-<<<<<<< HEAD
 import Sidebar from './Sidebar'
-=======
+
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"
 import Input from "./Input"
->>>>>>> 633bea9200a7fabd4c0b8372a006cbcb6cabe097
+
 
 export default class ContentArea extends PureComponent {
     render() {
         return (
             <div className="ContentArea">
-<<<<<<< HEAD
-                <div className="main">
-                    <Recipiecard />
-                    <Recipiecard />
-                    <Recipiecard />
-                    <Recipiecard />
-                </div>
-                <Sidebar/>
-                {/*
-                <Recipe/>*/}
-=======
+
 
                 <Router>
                     <Switch>
@@ -32,13 +21,11 @@ export default class ContentArea extends PureComponent {
                             <Recipiecard />
                             <Recipiecard />
                             <Recipe/>
+                            <Sidebar></Sidebar>
                         </Route>
 
                         <Route path="/addRecipe">
                             <Input/>
-
-                            
-
                         </Route>
                         
 
@@ -46,7 +33,6 @@ export default class ContentArea extends PureComponent {
                     </Switch>
                 </Router>
                
->>>>>>> 633bea9200a7fabd4c0b8372a006cbcb6cabe097
             </div>
         )
     }
